7ed1c4d26bf32cd7e76bd3502ed0916ed2b64075
dannyhuang <kuanhsuh@gmail.com>
2020-03-03 15:57:06 +0800

    Update CSS

--- template-parts/content-portfolio-sidebar.php
+++ template-parts/content-portfolio-sidebar.php
@@ -1,6 +1,6 @@
-<h3 class="section-heading sidebar-heading">
+<h4 class="section-heading sidebar-heading">
   所有作品
-</h3>
+</h4>
 <ul class="shadow rounded-b">
   <li><a class="sidebar-li" href="<?php echo esc_html(site_url("/portfolio")); ?>">所有作品<?php get_template_part('template-parts/content', 'svg-chevron'); ?></a>
     <!-- <li><a class="sidebar-li" href="#">網頁應用程式<?php get_template_part('template-parts/content', 'svg-chevron'); ?></a>
