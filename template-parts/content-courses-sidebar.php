8fc848c522474385fd609801f01d27db63ba5517
dannyhuang <kuanhsuh@gmail.com>
2020-12-08 15:41:28 +0800

    update sidebar

--- template-parts/content-courses-sidebar.php
+++ template-parts/content-courses-sidebar.php
@@ -10,6 +10,8 @@
   </li>
   <li><a class="sidebar-li" href="<?php echo esc_html(site_url("/free-courses")) ?>">免費資源<?php get_template_part('template-parts/content', 'svg-chevron'); ?></a>
   </li>
+  <li><a class="sidebar-li" href="https://blankslate.io?note=414638">加入我的聯盟計畫<?php get_template_part('template-parts/content', 'svg-chevron'); ?></a>
+  </li>
   <!-- <li><a class="sidebar-li" href="<?php echo esc_html(site_url("/wordpress-website")) ?>">線上輔導課程<?php get_template_part('template-parts/content', 'svg-chevron'); ?></a>
   </li>
   <li><a class="sidebar-li" href="<?php echo esc_html(site_url("/wordpress-website")) ?>">線上家教<?php get_template_part('template-parts/content', 'svg-chevron'); ?></a>
